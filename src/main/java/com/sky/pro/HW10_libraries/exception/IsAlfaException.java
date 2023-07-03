46189444d3280f57057eda1db7a570971e325e04
Elena <elenela@yandex.ru>
2023-07-04 02:22:16 +0300

    ДЗ 10!

--- src/main/java/com/sky/pro/HW10_libraries/exception/IsAlfaException.java
+++ src/main/java/com/sky/pro/HW10_libraries/exception/IsAlfaException.java
@@ -1,7 +1,13 @@
+
 package com.sky.pro.HW10_libraries.exception;
 
-public class IsAlfaException extends Exception {
+
+public class IsAlfaException extends RuntimeException {
+
     public IsAlfaException(String message) {
+
         super(message);
+
     }
+
 }
--- src/main/java/com/sky/pro/HW10_libraries/exception/IsAllLowerCaseException.java
+++ src/main/java/com/sky/pro/HW10_libraries/exception/IsAllLowerCaseException.java
@@ -1,8 +1,14 @@
+
 package com.sky.pro.HW10_libraries.exception;
 
-public class IsAllLowerCaseException extends Throwable {
+
+public class IsAllLowerCaseException extends RuntimeException {
+
 
     public IsAllLowerCaseException(String message) {
+
         super(message);
+
     }
+
 }
